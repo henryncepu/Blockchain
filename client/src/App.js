99c72a046336c5bb4e075421d20e8a6f6807eff6
henryncepu <51872921+henryncepu@users.noreply.github.com>
2020-08-27 14:54:55 +1000

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import StrawberryManager from "./contracts/StrawberryManager.json";
+import Strawberry from "./contracts/Strawberry.json";
+import getWeb3 from "./getWeb3";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import FoodiesApp from "./components/FoodiesApp";
+import { Package } from "./components/Package";
+import { Deliver } from "./components/deliver";
+import { ChangeStatusWarning } from "./components/ChangeStatusWarning";
+import { ChangeStatusUnfit } from "./components/unfitstatus";
+import { ChangeStatusGood } from "./components/statusmodify";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import firebase from "firebase";
+import SignInScreen from "./firebase-init";
+
+import "./App.css";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Switch>
+                        <Route path="/signedIn">
+                            <FoodiesApp />
+                        </Route>
+                        <Route path="/">
+                            <SignInScreen />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
+    }
+}
+
+export default App;
